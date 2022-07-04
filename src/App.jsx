0fc811d7e4573d5e124655a63f09e7a332e9653e
Yarden Ankonina <yardi1994@gmail.com>
2022-07-05 02:32:17 +0300

    reactr-outer

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+import * as React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+
 import './App.css';
 import Home from './pages/Home';
 import Favourites from './pages/Favourites';
@@ -5,10 +8,14 @@ import Favourites from './pages/Favourites';
 function App() {
   return (
     <div className="App">
-      <Home/>
-      <Favourites/>
+      <Routes>
+        <Route index  path="/" element={<Home />} />
+        <Route path="favourites" element={<Favourites />} />
+      </Routes>
     </div>
   );
 }
+
+
 
 export default App;
--- src/pages/Favourites.jsx
+++ src/pages/Favourites.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Favourites() {
   return (
-    <div>Favourites</div>
+    <>
+        <div>Favourites</div>
+        <Link to="/">Home</Link>
+    </>
   )
 }
