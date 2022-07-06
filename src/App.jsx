8981156e9c31c2c975d56d295447c4b7b3ee720b
Yarden Ankonina <yardi1994@gmail.com>
2022-07-07 00:11:35 +0300

    pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import { Routes, Route} from "react-router-dom";
 
 import './App.css';
 // import Home from './pages/home.jsx';
-import Favourites from './pages/favourites';
-import NotFound from "./pages/notFound";
+// import Favourites from './pages/favourites';
+// import NotFound from "./pages/notFound";
 import Layout from "./components/layout";
 import { ThemeProvider } from "./utils/themeProvider";
 
@@ -14,8 +14,8 @@ function App() {
           <Routes>
             <Route path="/" element={<Layout/>}>
               {/* <Route index  path="/" element={<Home/>} /> */}
-              <Route path="/favourites" element={<Favourites />} />
-              <Route path="/*" element={<NotFound/>} /> 
+              {/* <Route path="/favourites" element={<Favourites />} /> */}
+              {/* <Route path="/*" element={<NotFound/>} />  */}
             </Route>
           </Routes>
     </ThemeProvider>
