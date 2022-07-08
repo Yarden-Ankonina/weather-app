6d11cd5a9ce54f9bb896a795bbc0a729493c2d28
Yarden Ankonina <yardi1994@gmail.com>
2022-07-08 09:49:18 +0300

    media query

--- src/components/cityForecastExpanded.jsx
+++ src/components/cityForecastExpanded.jsx
@@ -5,7 +5,7 @@ import LocalForecastExpanded from './localForecastExpanded'
 export default function CityForecastExpanded() {
   return (
     <>
-        <div className='bg-slate-100 w-1/4 mx-2 dark:bg-slate-900/30 bg-opacity-20 dark:border-none border border-solid rounded'>
+        <div className='bg-slate-100 w-3/4 mx-2 lg:w-1/4 md:w-1/3 sm:w-1/2  dark:bg-slate-900/30 bg-opacity-20 dark:border-none border border-solid rounded'>
             <LocalForecastExpanded/>
             <FiveDayForecast/>
         </div>
