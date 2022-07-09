c75716d099dbc8f09a2376bf3d0e1e381507e906
Yarden Ankonina <yardi1994@gmail.com>
2022-07-09 14:32:30 +0300

    search options

--- src/components/searchLocation.jsx
+++ src/components/searchLocation.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import { useQueryClient } from 'react-query';
 
 
-export default function SearchLocation({searchQuery, setSearchQuery}) {
+export default function SearchLocation({searchQuery, setSearchQuery, onSearchClick}) {
   const queryClient = useQueryClient();
 
   const onChange = (e)=>{
@@ -13,8 +13,8 @@ export default function SearchLocation({searchQuery, setSearchQuery}) {
   
   return (
     <>
-    <div className='mt-2 flex flex-row'>
-        <input className='w-64 p-0.5 pl-2 bg-sky-50 dark:bg-zinc-800 border border-solid border-slate-800 dark:border-slate-500 dark:border-2 rounded dark:text-zinc-300'  type="text" placeholder='&#128269; Search For Location' onChange={onChange} value={searchQuery}/>
+    <div onKeyUp={onSearchClick} className='mt-2 flex flex-row '>
+        <input onClick={onSearchClick}  className='w-64 p-0.5 pl-2 bg-sky-50 dark:bg-zinc-800 border border-solid border-slate-800 dark:border-slate-500 dark:border-2 rounded dark:text-zinc-300'  type="text" placeholder='&#128269; Search For Location' onChange={onChange} value={searchQuery}/>
     </div>
     </>
   )
--- src/components/searchLocationContainer.jsx
+++ src/components/searchLocationContainer.jsx
@@ -8,19 +8,30 @@ import SearchOptions from './searchOptions';
 
 export default function SearchLocationContainer() {
   const [searchQuery, setSearchQuery] = useState("");
+  const [showSearchOptions, setShowSearchOptions] = useState(true);
+
+
   const queryClient = useQueryClient();
-  const {data,status} = useQuery(["inputQuery",searchQuery] , fetchAutoCompleteCities)
-  let showSearchOptions = false;
-  console.log(status)
-  if(status === 'success'){
-    showSearchOptions = true;
+  const {data,status,isFetching, refetch } = useQuery(["inputQuery",searchQuery] , fetchAutoCompleteCities,{
+    manual:true,
+    enabled:false,
+    
+  });
+
+  const onSearchClick = ()=>{
+    if(!isFetching){
+      refetch();
+    }
+    if(status === 'success'){
+      setShowSearchOptions(true);
+    }
   }
 
   return (
     <>
     <div className='flex flex-col w-full justify-center items-center'>
-      <SearchLocation setSearchQuery={setSearchQuery} value={searchQuery}/>
-      <SearchOptions data={data} showSearchOptions={showSearchOptions}/>
+      <SearchLocation setSearchQuery={setSearchQuery} value={searchQuery} onSearchClick={onSearchClick}/>
+      <SearchOptions data={data} showSearchOptions={showSearchOptions} setShowSearchOptions={setShowSearchOptions}/>
     </div>
     </>
   )
