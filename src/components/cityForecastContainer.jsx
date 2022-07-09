49d1ad549b0a2cfc08db772bae09341ad790895d
Yarden Ankonina <yardi1994@gmail.com>
2022-07-09 21:30:07 +0300

    city Container

--- src/components/cityForecastContainer.jsx
+++ src/components/cityForecastContainer.jsx
@@ -2,11 +2,16 @@ import React from 'react'
 import { useGeoPosition } from '../utils/mockServerJsonService'
 
 import CityForecast from './cityForecast'
+import SearchLocationContainer from './searchLocationContainer'
 
 export default function CityForecastContainer() {
   return (
     <>
-      <CityForecast/>
+        <SearchLocationContainer/>
+        <button className='text-6xl mb-2 font-light text-red-500' onClick={toggleHeart}>&#9825;</button>
+        <div className='flex w-full justify-center items-start '>
+        <CityForecast/>
+        </div>
     </>
   )
 }
