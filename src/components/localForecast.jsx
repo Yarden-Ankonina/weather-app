4c5c8f49b8e06008696e4299c6c6ad3c87480de8
Yarden Ankonina <yardi1994@gmail.com>
2022-07-09 18:58:30 +0300

    city creation

--- src/components/localForecast.jsx
+++ src/components/localForecast.jsx
@@ -1,46 +1,42 @@
-import React from 'react'
-import { GetPCheck } from '../utils/weatherService';
+import React from 'react';
+import {useQuery, useQueryClient} from 'react-query';
 
-export default function LocalForecast() {
-    const cityForcast = GetPCheck()
-    const isCelsius = true;
+import { mockFetchCurrentWeather, mockFetchCityByLocationKey } from '../utils/mockServerJsonService';
+import { GetPCheck, fetchCurrentConditions } from '../utils/weatherService';
+
+export default function LocalForecast({city}) {
+    const onClick = ()=>{
+
+    }
   return (
     <>
         <div className='flex flex-col items-start p-2 rounded-xl dark:text-slate-200 bg-cyan-50/60 border-solid border-cyan-800 dark:bg-sky-800 shadow-md dark:shadow-sm dark:shadow-slate-400 relative' >
-            <button className='absolute right-0 top-0 w-7 h-7 align-middle text-center hover:text-slate-700'>&#8451;</button>
+            <button onClick={onClick} className='absolute right-0 top-0 w-7 h-7 align-middle text-center hover:text-slate-700'>&#8451;</button>
             <div className='flex border-b w-full border-solid'>
                <img className='w-16 h-10' src='https://developer.accuweather.com/sites/default/files/01-s.png'/>
-                {isCelsius 
-                    ?<div className='text-3xl font-sans font-medium'>{cityForcast.forecast.Temperature.Metric.Value}<span>&deg;</span></div>
-                    :<div className='text-3xl font-sans font-medium'>{cityForcast.forecast.Temperature.Imperial.Value}&deg;</div>
+                {city.isCelsius 
+                    ?<div className='text-3xl font-sans font-medium'>{city.temperture.celsius}<span>&deg;</span></div>
+                    :<div className='text-3xl font-sans font-medium'>{city.temperture.fahrenheit}&deg;</div>
                 }
             </div>
             <div className='flex w-full p-1 shadow justify-between'>
                 <div >Current Weather</div>
-                {isCelsius 
-                ?<div className='font-medium'>{cityForcast.forecast.Temperature.Metric.Value}<span>&deg;</span></div>
-                :<div className='font-medium'>{cityForcast.forecast.Temperature.Imperial.Value}&deg;</div>
+                {city.isCelsius  
+                ?<div className='font-medium'>{city.temperture.celsius}<span>&deg;</span></div>
+                :<div className='font-medium'>{city.temperture.fahrenheit}&deg;</div>
                 }
             </div>
             <div className='flex w-full p-1 shadow justify-between'>
-                <div>Time</div>
-                <div className=''>12:30</div>
-            </div>
-            <div className='flex w-full p-1 shadow justify-between'>
-                <div>Air Quality</div>
-                <div className=''>Fair</div>
-            </div>
-            <div className='flex w-full p-1 shadow justify-between'>
                 <div>City</div>
-                <div className=' text-slate-900 dark:text-slate-200'>{cityForcast.name}</div>
+                <div className=' text-slate-900 dark:text-slate-200'>{city.cityName}</div>
             </div>
             <div className='flex w-full p-1 shadow justify-between'>
                 <div>Country/State</div>
-                <div className='text-slate-800 dark:text-slate-300'>{cityForcast.country}</div>
+                <div className='text-slate-800 dark:text-slate-300'>{city.countryName}</div>
             </div>
             <div className='flex w-full p-1 shadow justify-between'>
-                <div>Humidity</div>
-                <div>47&deg;</div>
+             <div></div>
+                <a className=' flex text-slate-800 dark:text-slate-300' href={`${city.Link}`} target="_blank">   <div className='mr-2'>More Information</div><img className='w-7 h-7 flex justify-center items-center' src="https://img.icons8.com/windows/344/external-link-squared.png"/></a>
             </div>
         </div>
     </>
